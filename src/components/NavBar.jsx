15799b15f7684ea5113c8aec11cd899bd8c10211
Kade <kadekorn@gmail.com>
2023-09-26 11:29:50 -0500

    adding links to the tesla page for each of the nav items such as vehicles and solar and shop/account/menu. With that I have been coding for around another hour, so its been 2.5 hours. Lets see what more I can do

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,45 +4,60 @@ export const NavBar = () => {
   return (
     <div className="flex justify-between items-center px-12 p-4 text-sm font-bold">
       <div>
-        <img
-          className="h-3"
-          src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0idGRzLWljb24gdGRzLWljb24tbG9nby13b3JkbWFyayB0ZHMtc2l0ZS1sb2dvLWljb24iIHZpZXdCb3g9IjAgMCAzNDIgMzUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTAgLjFhOS43IDkuNyAwIDAgMCA3IDdoMTFsLjUuMXYyNy42aDYuOFY3LjNMMjYgN2gxMWE5LjggOS44IDAgMCAwIDctN0gwem0yMzguNiAwaC02Ljh2MzQuOEgyNjNhOS43IDkuNyAwIDAgMCA2LTYuOGgtMzAuM1Ywem0tNTIuMyA2LjhjMy42LTEgNi42LTMuOCA3LjQtNi45bC0zOC4xLjF2MjAuNmgzMS4xdjcuMmgtMjQuNGExMy42IDEzLjYgMCAwIDAtOC43IDdoMzkuOXYtMjFoLTMxLjJ2LTdoMjR6bTExNi4yIDI4aDYuN3YtMTRoMjQuNnYxNGg2Ljd2LTIxaC0zOHpNODUuMyA3aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxMy44aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxNC4xaDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3pNMzA4LjUgN2gyNmE5LjYgOS42IDAgMCAwIDctN2gtNDBhOS42IDkuNiAwIDAgMCA3IDd6IiBmaWxsPSJjdXJyZW50Q29sb3IiLz48L3N2Zz4="
-          alt="Tesla-Logo"
-        />
+        <a href="https://tesla.com">
+          <img
+            className="h-3"
+            src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0idGRzLWljb24gdGRzLWljb24tbG9nby13b3JkbWFyayB0ZHMtc2l0ZS1sb2dvLWljb24iIHZpZXdCb3g9IjAgMCAzNDIgMzUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTAgLjFhOS43IDkuNyAwIDAgMCA3IDdoMTFsLjUuMXYyNy42aDYuOFY3LjNMMjYgN2gxMWE5LjggOS44IDAgMCAwIDctN0gwem0yMzguNiAwaC02Ljh2MzQuOEgyNjNhOS43IDkuNyAwIDAgMCA2LTYuOGgtMzAuM1Ywem0tNTIuMyA2LjhjMy42LTEgNi42LTMuOCA3LjQtNi45bC0zOC4xLjF2MjAuNmgzMS4xdjcuMmgtMjQuNGExMy42IDEzLjYgMCAwIDAtOC43IDdoMzkuOXYtMjFoLTMxLjJ2LTdoMjR6bTExNi4yIDI4aDYuN3YtMTRoMjQuNnYxNGg2Ljd2LTIxaC0zOHpNODUuMyA3aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxMy44aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxNC4xaDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3pNMzA4LjUgN2gyNmE5LjYgOS42IDAgMCAwIDctN2gtNDBhOS42IDkuNiAwIDAgMCA3IDd6IiBmaWxsPSJjdXJyZW50Q29sb3IiLz48L3N2Zz4="
+            alt="Tesla-Logo"
+          />
+        </a>
       </div>
 
       <div className="hidden lg:inline">
         <ul className="flex justify-center hover:cursor-pointer">
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">Model S</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">Model 3</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">Model X</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">Model Y</li>
           <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">
-            Solar Roof
+            {" "}
+            <a href="https://tesla.com/models">Model S</a>
           </li>
           <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">
-            Solar Panels
+            <a href="https://tesla.com/model3"> Model 3</a>
+          </li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">
+            <a href="https://tesla.com/modelx">Model X</a>
+          </li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">
+            <a href="https://tesla.com/modely">Model Y</a>
+          </li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">
+            <a href="https://tesla.com/solarroof">Solar Roof</a>
+          </li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">
+            <a href="https://tesla.com/solarpanels">Solar Panels </a>
           </li>
         </ul>
       </div>
 
       <div className="hidden lg:inline">
         <ul className="flex justify-center hover:cursor-pointer">
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">Shop</li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">
+            <a href="https://shop.tesla.com">Shop</a>
+          </li>
           <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">Account</li>
           <li className="py-1 px-3 hover:rounded hover:bg-black/5 ">Menu</li>
         </ul>
       </div>
       <div className="lg:hidden">
-        <button className="inline-flex items-center rounded-md py-2 px-4 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10">Menu</button>
+        <button className="inline-flex items-center rounded-md py-2 px-4 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10">
+          Menu
+        </button>
       </div>
     </div>
   );
 };
 
-//Component explained - GIVE BETTER DESCRIPTION WHEN DONE Whats really cool about using tailwind is you can make your website so responsive quite easily. 
+//Component explained - GIVE BETTER DESCRIPTION WHEN DONE Whats really cool about using tailwind is you can make your website so responsive quite easily.
 
-// Starting Div (container) css explained 
+// Starting Div (container) css explained
 // flex: Applies the CSS flex display property, making the element a flex container.
 // justify-between: Horizontally spaces flex items such that the first item is at the start and the last item is at the end of the flex container.
 // items-center: Vertically aligns flex items to the center of the flex container.
@@ -62,7 +77,7 @@ export const NavBar = () => {
 // justify-center: Horizontally aligns flex items to the center of the flex container.
 // hover:cursor-pointer: Changes the mouse cursor to a pointer (hand icon) when the element is hovered over.
 
-// Button css explained 
+// Button css explained
 // inline-flex: Applies the CSS inline-flex display property, making the element an inline-level flex container.
 // items-center: Vertically aligns flex items to the center of the flex container.
 // rounded-md: Rounds the corners of the element with a medium radius.
