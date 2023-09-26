426e25ae3c93eabdefa166f2678785b608d3bea1
Kade <kadekorn@gmail.com>
2023-09-26 07:33:44 -0500

    finished all the HTML, structure to needed icons. Now on to styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import telsa from './assets/tesla.svg'
+import { Model } from './components/Model';
 import { NavBar } from './components/NavBar';
 
 
@@ -6,8 +6,10 @@ function App() {
   return (
     <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/9160c5a3-b818-42dd-bb98-b8597948c636/bvlatuR/std/1927x4096/M3-Homepage-Mobile-LHD')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/9160c5a3-b818-42dd-bb98-b8597948c636/bvlatuR/std/1927x4096/M3-Homepage-Mobile-LHD')] h-screen bg-cover bg-center ">
     <NavBar />
+    <Model />
     </div>
   );
 }
 
 export default App;
+// REACT | TAILWIND | FIREBASE
---
+++ src/components/Model.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FiChevronDown } from "react-icons/fi";
+
+export const Model = () => {
+  return (
+    <div>
+      <div>
+        <h1>Model 3</h1>
+        <p>Order online for Touchless Delivery</p>
+      </div>
+
+      <div>
+        <button>Custom Order</button>
+        <button>Existing Inventory</button>
+      </div>
+      <div>
+        <FiChevronDown />
+      </div>
+    </div>
+  );
+};
