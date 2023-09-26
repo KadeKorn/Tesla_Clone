6972aeb1803f28e909796ff2480cd8176de4c8ba
Kade <kadekorn@gmail.com>
2023-09-26 06:40:30 -0500

    going well. took me forever to find the proper quality tesla image, but we got it . Here is the tailwind I am using :h-screen: Sets the element's height to be the same as the height of the viewport. bg-cover: Scales the background image to cover the entire element, cropping it if necessary. bg-center: Positions the background image at the center of the element.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import telsa from './assets/tesla.svg'
+
+
 function App() {
   return (
-    <div className="">
+    <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/9160c5a3-b818-42dd-bb98-b8597948c636/bvlatuR/std/1927x4096/M3-Homepage-Mobile-LHD')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/9160c5a3-b818-42dd-bb98-b8597948c636/bvlatuR/std/1927x4096/M3-Homepage-Mobile-LHD')] h-screen bg-cover bg-center ">
       <h1 className="text-3xl font-bold underline">Tesla Clone</h1>
     </div>
   );
